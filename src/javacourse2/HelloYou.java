508d2bd5cad074a3c80791fd5e48acede2b7c441
gustavooarantes <profissional.gustavoa@gmail.com>
2025-05-14 19:20:52 -0300

    Trying the GCM

---
+++ src/javacourse2/HelloYou.java
@@ -0,0 +1,15 @@
+package javacourse2;
+
+import java.util.Scanner;
+
+public class HelloYou {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " + name);
+        scanner.close();
+    }
+}
