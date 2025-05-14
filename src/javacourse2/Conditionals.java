fd09489ad0571b447edd085831f85c5924e92af5
gustavooarantes <profissional.gustavoa@gmail.com>
2025-05-14 20:38:02 -0300

    Day two

--- src/javacourse2/Conditionals.java
+++ src/javacourse2/Conditionals.java
@@ -1,4 +1,14 @@
 package javacourse2;
 
 public class Conditionals {
+    public static void main(String[] args) {
+        // Use case of a simple if-then case (no use of else cases) - not a particularly good practice
+        boolean isValid = false;
+        if (!isValid)
+            System.out.println("It is false");
+
+        // Ternary operator
+        isValid = true;
+        String myText = isValid ? "This if true" : "This if false";
+    }
 }
